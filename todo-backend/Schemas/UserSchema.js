3e00fff40a08cd353c6c8002bf491510865262d4
alperen-bircak <bircakalperen@gmail.com>
2023-05-13 19:17:56 +0300

    Register endpoint

---
+++ todo-backend/Schemas/UserSchema.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password_hash: {
+    type: String,
+    required: true
+  }
+});
+const User = mongoose.model('User', userSchema)
+export default User;
--- todo-backend/index.js
+++ todo-backend/index.js
@@ -1,4 +1,7 @@
+
+
 import app from "./app.js";
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Api is running on port: ${process.env.PORT}`);
