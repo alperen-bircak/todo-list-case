ab69b8968f277f121d7d4f48a952820dff5bf8dc
alperen-bircak <bircakalperen@gmail.com>
2023-05-15 21:34:49 +0300

    Todo edit added

--- todo-frontend/src/Components/TodoView/TodoView.jsx
+++ todo-frontend/src/Components/TodoView/TodoView.jsx
@@ -1,11 +1,14 @@
-import { DeleteOutlined } from "@ant-design/icons";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "antd";
-import React from "react";
+import React, { useState } from "react";
 import "./TodoView.scss";
 import { axios } from "axios";
+import ClickAwayListener from "react-click-away-listener";
+import TodoForm from "../TodoForm/TodoForm";
 const TodoView = ({ todo, instance }) => {
   const queryClient = useQueryClient();
+  const [edit, setEdit] = useState(false);
 
   const deleteItem = useMutation(
     async () => {
@@ -18,11 +21,25 @@ const TodoView = ({ todo, instance }) => {
     }
   );
 
+  if (edit) {
+    return (
+      <ClickAwayListener onClickAway={() => setEdit(false)}>
+        <div className="todo-view">
+          <TodoForm />
+        </div>
+      </ClickAwayListener>
+    );
+  }
   if (todo) {
     return (
       <div className="todo-view">
         <div className="todo-body">{todo.body}</div>
         <div className="buttons">
+          <Button
+            shape="circle"
+            icon={<EditOutlined />}
+            onClick={() => setEdit(true)}
+          />
           <Button
             shape="circle"
             icon={<DeleteOutlined />}
